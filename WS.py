d5ea7fedaa10f1f6412c7d8f8f89c423f0289206
Meeran54 <meeranqazi77@gmail.com>
2025-06-05 19:29:46 +0530

    Add files via upload

---
+++ WS.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "856881c1-de5a-4692-bc25-c9f1fda082fa",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
